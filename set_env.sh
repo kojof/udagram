35a5c51dcf27963451e8cd59b3fc132e48f2b006
kojo frempong <kojof@hotmail.com>
2020-06-25 11:54:52 +0100

    First Draft
    First draft

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udagramkojo
+export POSTGRES_PASSWORD=Frempong1
+export POSTGRES_HOST=udagramkojo.cr793kleprqw.eu-west-2.rds.amazonaws.com
+export POSTGRES_DB=udagramkojo
+export AWS_BUCKET=udagram-kojof-dev
+export AWS_REGION=eu-west-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
